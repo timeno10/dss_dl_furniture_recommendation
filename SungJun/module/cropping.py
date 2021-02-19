16906a0b3784dde0f6af27bb7d0a7c27a4196668
SungJun Kim <71831714+alltimeno1@users.noreply.github.com>
2021-02-19 10:49:32 +0900

    Use Colab

--- SungJun/module/cropping.py
+++ SungJun/module/cropping.py
@@ -1,10 +1,9 @@
-import torch, torchvision
-assert torch.__version__.startswith("1.7")
-import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
+# !pip install pyyaml==5.1
+# !pip install Pillow==5.3.0
+# !pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.7/index.html
+
+import torch, torchvision, cv2, detectron2
 import numpy as np
-import cv2
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
@@ -13,15 +12,17 @@ from detectron2.data import MetadataCatalog
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-cfg.MODEL.WEIGHTS = "model_final.pkl"
+cfg.MODEL.WEIGHTS = "/content/drive/MyDrive/Colab Notebooks/dl_project/model_final.pkl"
 
 predictor = DefaultPredictor(cfg)
-im = cv2.imread("pics/input.jpg")
+im = cv2.imread("/content/drive/MyDrive/Colab Notebooks/dl_project/room.jpg")
 outputs = predictor(im)
+n = 0
 
 for cls, xy in zip(outputs["instances"].pred_classes, outputs["instances"].pred_boxes):
-    if cls == 56:
+    obj = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes[cls]
+    if obj in ['chair', 'couch', 'clock']:
+        n += 1
         xy = list(map(round, xy.tolist()))
         dst = im[xy[1]:xy[3], xy[0]:xy[2]].copy()
-        obj = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).thing_classes[cls]
-        cv2.imwrite("pics/input.jpg".format(obj), dst)
+        cv2.imwrite("/content/drive/MyDrive/Colab Notebooks/dl_project/{}_{}.jpg".format(obj, n), dst)
